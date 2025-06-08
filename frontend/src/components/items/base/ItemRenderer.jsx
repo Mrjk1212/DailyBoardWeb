cf56877f5be02d90882e9da1ffcf00974f66a7d9
TrippyHippy <spidermanalc@gmail.com>
2025-06-07 21:45:41 -0500

    Fix: Lag issue with creating props

--- frontend/src/components/items/base/ItemRenderer.jsx
+++ frontend/src/components/items/base/ItemRenderer.jsx
@@ -3,27 +3,52 @@ import StickyNote from "../sticky-note/StickyNote";
 import TodoList from "../todo-list/TodoList";
 import { ITEM_TYPES } from "../../../constants/itemTypes";
 
-const ItemRenderer = ({ item, isSelected, onDragEnd, onSelect, onDoubleClick, onResize, isDraggable, onDelete, onUpdate }) => {
-    const commonProps = {
-        item,
-        isSelected,
-        onDragEnd,
-        onSelect,
-        onDoubleClick,
-        onResize,
-        isDraggable,
-        onDelete,
-        onUpdate,
-    };
-
+const ItemRenderer = React.memo(({ 
+    item, 
+    isSelected, 
+    onDragEnd, 
+    onSelect, 
+    onDoubleClick, 
+    onResize, 
+    isDraggable, 
+    onDelete, 
+    onUpdate 
+}) => {
+    // Don't recreate commonProps object - pass props directly (somehow this creates new stuff for every re-render and is slow AF)
     switch (item.type) {
         case ITEM_TYPES.STICKY_NOTE:
-            return <StickyNote {...commonProps} />;
+            return (
+                <StickyNote
+                    item={item}
+                    isSelected={isSelected}
+                    onDragEnd={onDragEnd}
+                    onSelect={onSelect}
+                    onDoubleClick={onDoubleClick}
+                    onResize={onResize}
+                    isDraggable={isDraggable}
+                    onDelete={onDelete}
+                    onUpdate={onUpdate}
+                />
+            );
         case ITEM_TYPES.TODO_LIST:
-            return <TodoList {...commonProps} />;
+            return (
+                <TodoList
+                    item={item}
+                    isSelected={isSelected}
+                    onDragEnd={onDragEnd}
+                    onSelect={onSelect}
+                    onDoubleClick={onDoubleClick}
+                    onResize={onResize}
+                    isDraggable={isDraggable}
+                    onDelete={onDelete}
+                    onUpdate={onUpdate}
+                />
+            );
         default:
             return null;
     }
-};
+});
+
+ItemRenderer.displayName = 'ItemRenderer';
 
 export default ItemRenderer;
